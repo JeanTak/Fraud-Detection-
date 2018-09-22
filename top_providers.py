ab7581da9a7a6a718d5f0167ca3e1a0700c4f16a
Jeantak <kanyoo.wu@mail.utoronto.ca>
2018-09-22 00:56:35 -0400

    top_providers changed

--- top_providers.py
+++ top_providers.py
@@ -1,16 +1,22 @@
 from calculate_outlieness import calculate_outlieness
+import operator
 #5. list[list[int and str]] top_100_providers(list[float] outlieness, list[list[float]] eight_dimensional_data);
 
 def top_providers(outlieness, eight_dimensional_data):	
 	provider_list = {}
 	outlieness = calculate_outlieness(eight_dimensional_data)
 
-	for i in range(len(eight_dimensional_data)):
-		provider_list[eight_dimensional_data[i][2]] += 1
-	
-	sorted(provider_list, key = lambda x: x[1], reverse=True)
+	for i in range(len(outlieness)):
+		provider_list[eight_dimensional_data[i][2]] += outlieness[i]
+		
 
-	rank = {key: rank for rank, key in enumerate(sorted(provider_list, key=provider_list.get, reverse=True), 1)}	
+	sorted_list = sorted(provider_list.items(), key=operator.itemgetter(1), reverse=True)
+	providers = sorted_list.keys()
+
+	rank = {}
+	for i in range(len(providers)):
+		rank[providers[i]] = i
+	
 	return rank
 
 	
