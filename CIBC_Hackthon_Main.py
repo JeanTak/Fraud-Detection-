7fd6def36568e4e590d544472cadf7959351abce
wbh123456 <35285095+wbh123456@users.noreply.github.com>
2018-09-21 23:34:03 -0400

    Add files via upload

--- CIBC_Hackthon_Main.py
+++ CIBC_Hackthon_Main.py
@@ -6,10 +6,13 @@ from find_k_nearest_neighbours import find_k_nearest_neighbours
 from calculate_outlieness import calculate_outlieness
 from top_providers import top_providers
 
-all_points = [[]]
-all_points = read_csv_file("5_dimension(200row)")
+five_dimensional_points = [[]]
+eight_dimensional_points = [[]]
+five_dimensional_points = read_csv_file("5_dimension(200row)")
+eight_dimensional_points = read_csv_file("Full_file(200row)")
+
 outlieness = []
-outlieness = calculate_outlieness(all_points)
+outlieness = calculate_outlieness(five_dimensional_points)
 
 top_providers_list = [[]]
-top_providers_list= top_providers(outlieness)
+top_providers_list= top_providers(outlieness, eight_dimensional_points)
--- top_providers.py
+++ top_providers.py
@@ -1,4 +1,4 @@
-#5. list[list[int and str]] top_100_providers(list[float] outlieness);
+#5. list[list[int and str]] top_100_providers(list[float] outlieness, list[list[float]] eight_dimensional_data);
 
-def top_providers(outlieness):
+def top_providers(outlieness, eight_dimensional_data):
     return [[]]
